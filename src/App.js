4e0ef8f2b4321e65df7236fb5aff08ca298f1d28
Sai Krishna Mukund <mukundsai79@github.com>
2024-11-22 16:27:00 -0800

    Saving progress before resolving rebase

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+<<<<<<< HEAD
 //App js file
+=======
+import "./App.css";
+
+function App() {
+  return <div>GitHub API Visualizations</div>;
+}
+
+export default App;
+>>>>>>> f663161 (Initial folders commit)
