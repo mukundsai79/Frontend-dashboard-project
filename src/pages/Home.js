a5c4fb03b8ec0e4cae9ed3293dc2500ad5c257d1
emmayakley <emmie.yakley@gmail.com>
2024-11-23 23:11:34 -0800

    finished top navbar basic implementation

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+//Homepage code will go here
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Github API Visualizations</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Search.js
@@ -0,0 +1,13 @@
+//Page for searching for a github user and outputting the information returned from the API will go here
+
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <h1>Github User Search</h1>
+    </div>
+  );
+};
+
+export default Search;
